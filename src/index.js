9479ea795b678c6e1be4a8ba24224f47324df7bf
Lisa <118001463+Lilo002@users.noreply.github.com>
2023-01-30 17:47:52 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+  n = Math.abs(n);
+    let str = n.toString();
+    let arr = str.split('');
+    let arr1 = [];
+    while (arr.length) {
+        arr1.unshift(arr.splice(0,1));
+    }
+    if (arr1[0] == 0) {
+        arr1.shift()
+    }    
+    return +(arr1.join(''));
 }
